9ac1a38b2ff1cb98bee6bd9c488b8c86bc44a789
farukvaiy199-netizen <farukvaiy199@gmail.com>
2026-08-04 15:03:28 +0600

    Add fbAppSecret to configuration

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT || 3000,
   webhookVerifyToken: process.env.WEBHOOK_VERIFY_TOKEN || 'change_me',
-  encryptionKey: process.env.SETTINGS_ENCRYPTION_KEY || 'change_this_to_a_long_random_string'
+  encryptionKey: process.env.SETTINGS_ENCRYPTION_KEY || 'change_this_to_a_long_random_string',
+  fbAppSecret: process.env.FB_APP_SECRET || ''
 };
