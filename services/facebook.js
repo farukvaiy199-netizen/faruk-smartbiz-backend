0ccae6ffcfacbe2b1654728eb94d31c346527f7f
farukvaiy199-netizen <farukvaiy199@gmail.com>
2026-08-05 12:44:59 +0600

    Implement getFacebookProfile function
    Add function to retrieve Facebook profile name by PSID

--- services/facebook.js
+++ services/facebook.js
@@ -8,4 +8,17 @@ async function sendFacebookMessage(recipientId, text, pageAccessToken) {
   }, { timeout: 10000 });
 }
 
-module.exports = { sendFacebookMessage };
+// কাস্টমারের PSID দিয়ে তার নাম আনে (Messenger প্রোফাইল লিংক দেয় না, শুধু নাম/ছবি দেয়)
+async function getFacebookProfile(psid, pageAccessToken) {
+  try {
+    const url = `https://graph.facebook.com/v19.0/${psid}?fields=first_name,last_name&access_token=${pageAccessToken}`;
+    const res = await axios.get(url, { timeout: 8000 });
+    const name = [res.data.first_name, res.data.last_name].filter(Boolean).join(' ');
+    return name || null;
+  } catch (err) {
+    console.error('Facebook প্রোফাইল আনতে ব্যর্থ:', err.message);
+    return null;
+  }
+}
+
+module.exports = { sendFacebookMessage, getFacebookProfile };
