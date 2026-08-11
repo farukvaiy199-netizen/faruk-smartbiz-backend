3394d8232705d5747f79348e9517911206662ef7
farukvaiy199-netizen <farukvaiy199@gmail.com>
2026-08-11 06:32:13 +0600

    Implement getWhatsAppMediaBuffer function
    Added a new function to retrieve WhatsApp media as a buffer.

--- services/whatsapp.js
+++ services/whatsapp.js
@@ -13,7 +13,7 @@ async function sendWhatsAppMessage(to, text, phoneNumberId, token) {
   });
 }
 
-// প্রোডাক্টের ছবি পাঠানোর জন্য (নতুন ফিচার)
+// প্রোডাক্টের ছবি পাঠানোর জন্য
 async function sendWhatsAppImage(to, imageUrl, phoneNumberId, token, caption) {
   const url = `https://graph.facebook.com/v19.0/${phoneNumberId}/messages`;
   await axios.post(url, {
@@ -27,4 +27,23 @@ async function sendWhatsAppImage(to, imageUrl, phoneNumberId, token, caption) {
   });
 }
 
-module.exports = { sendWhatsAppMessage, sendWhatsAppImage };
+// WhatsApp-এ কাস্টমারের পাঠানো ছবি/ভয়েস মেসেজ সরাসরি লিংক আকারে আসে না, শুধু একটা media ID আসে।
+// প্রথমে সেই ID দিয়ে আসল (সাময়িক) ডাউনলোড লিংক আনতে হয়, তারপর টোকেন দিয়ে সেই লিংক থেকে ফাইল আনতে হয়।
+async function getWhatsAppMediaBuffer(mediaId, token) {
+  const metaRes = await axios.get(`https://graph.facebook.com/v19.0/${mediaId}`, {
+    headers: { Authorization: `Bearer ${token}` },
+    timeout: 10000
+  });
+  const mediaUrl = metaRes.data.url;
+  const mimeType = metaRes.data.mime_type;
+
+  const fileRes = await axios.get(mediaUrl, {
+    headers: { Authorization: `Bearer ${token}` },
+    responseType: 'arraybuffer',
+    timeout: 20000
+  });
+
+  return { buffer: Buffer.from(fileRes.data), mimeType };
+}
+
+module.exports = { sendWhatsAppMessage, sendWhatsAppImage, getWhatsAppMediaBuffer };
